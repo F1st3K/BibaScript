19180a2b34f506a9871dcbb35d64bb2a961beb7d
F1st3K <n1k0s04k@gmail.com>
2023-05-04 09:07:33 +0300

    first commit

---
+++ BibaScript/BibaScript/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BibaScript
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (var file in args)
+            {
+                try
+                {
+                    View.ColorWriteLine(file, ConsoleColor.Cyan);
+                }
+                catch (Exception ex)
+                {
+                    View.ColorWriteLine(ex.Message, ConsoleColor.Red);
+                    continue;
+                }
+            }
+        }
+
+        
+    }
+}
